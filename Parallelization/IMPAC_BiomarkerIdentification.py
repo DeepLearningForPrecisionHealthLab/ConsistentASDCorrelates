ec9e1575ab743f40b464483bc6e837feb903ba31
Cooper Mellema <cooper.mellema@utsouthwestern.edu>
2019-05-09 15:21:26 -0500

    WIPS commit, permutation testing stuff

---
+++ Parallelization/IMPAC_BiomarkerIdentification.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" ***Purpose*** 
+
+This file *** 
+    Example:
+    
+***Details***
+    Subheadings
+    
+Originally developed for use in the *** project
+Created by Cooper Mellema on 25 Apr, 2019
+in Dr. Montillo's Deep Learning Lab
+University of Texas Southwestern Medical Center
+Lyda Hill Dept. of Bioinformatics
+"""
+
+__author__ = "Cooper Mellema"
+__email__ = "Cooper.Mellema@UTSouthwestern.edu"
+
+__status__ = "Prototype"
